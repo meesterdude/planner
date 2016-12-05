bc7cb9fdcd4e2a5486e5ad8353c28d4031f1b723
Prelang Builder <builder@prelang.com>
2016-12-05 02:36:28 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :scores
+  resources :scores do
+  #->Prelang (voting/acts_as_votable)
+  member do
+    get "vote"
+  end
+end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
