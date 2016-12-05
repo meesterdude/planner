cabbbd6296cffc88a005eee8ccbd212a655cb840
Prelang Builder <builder@prelang.com>
2016-12-05 02:36:28 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,5 @@
 class ScoresController < ApplicationController
-  before_action :set_score, only: [:show, :edit, :update, :destroy]
+  before_action :set_score, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /scores
   # GET /scores.json
